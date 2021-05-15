8ac5383b221e46d8f30a2c810517e79c399e765d
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2021-05-15 23:48:11 +0700

    commit react_todo_list

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <>
+      <div className='todo-app'>
+        <TodoList />
+      </div>
+
+    </>
+  );
+}
+
+export default App;
